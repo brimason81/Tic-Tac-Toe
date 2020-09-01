f08bbb016e66bae09a314f0e4d07a34ed0f09036
brimason81 <34803524+brimason81@users.noreply.github.com>
2020-08-31 21:02:05 -0400

    Add files via upload

---
+++ TicTacToe/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div');
+    const playerDisplay = document.querySelector('#player');
+    let currentPlayer = "Player X";
+
+    squares.forEach( square => {
+        square.addEventListener('click', clickOutcome);
+    })
+
+    function clickOutcome(e) {
+        const squareArray = Array.from(squares);
+        const index = squareArray.indexOf(e.target);
+        //console.log(index);
+        playerDisplay.innerHTML = currentPlayer;
+
+        if (currentPlayer === 'playerX') { // === is deeply equals; gives boolean response
+            squares[index].classList.add('playerX');  
+            currentPlayer = "playerO";
+        } else {
+            squares[index].classList.add('playerO');
+            currentPlayer = "playerX";
+        }
+        
+    }
+});
